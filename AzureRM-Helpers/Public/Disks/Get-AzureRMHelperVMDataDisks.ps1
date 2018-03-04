4fc42096626eb22115b0ecd10ed27a1841a0f7d6
Oliver Li <oliver.1i@outlook.com>
2018-03-04 16:25:24 +0000

    Update Get-AzureRMHelperVMDataDisks function
    Modified Get-AzureRMHelperVMDataDisks function retrieve unmanaged data disks first from the same storage account as the OS Disk.

--- AzureRM-Helpers/Public/Disks/Get-AzureRMHelperVMDataDisks.ps1
+++ AzureRM-Helpers/Public/Disks/Get-AzureRMHelperVMDataDisks.ps1
@@ -45,21 +45,57 @@ Function Get-AzureRMHelperVMDataDisks
     }
     else
     {   #Find associated Unmanaged Data Disks
+        $osDiskStorageAccountName = (Get-AzureRMHelperVhdInfoFromUri $azureRMVM.StorageProfile.OsDisk.Vhd.Uri).StorageAccountName
         $storageAccounts = Get-AzureRmStorageAccount
+        #Check the storage account which contains the OS disk first        
+        $osDiskStorageAccount = $storageAccounts | Where-Object {$_.StorageAccountName -eq $osDiskStorageAccountName}
+        $remainingDisks = $azureRMVM.StorageProfile.DataDisks
         foreach($vhd in $azureRMVM.StorageProfile.DataDisks)
         {
-            $vhdBlob = $storageAccounts | Get-AzureStorageContainer | Get-AzureStorageBlob | Where-Object {$_.Name -match "$($vhd.Name)\.vhd"}
-            $rmVMDataDiskDetails += $vhdBlob
-            if($vhdBlob.GetType() -eq 'Object[]')
+            $vhdBlob = $osDiskStorageAccount | Get-AzureStorageContainer | Get-AzureStorageBlob | Where-Object {$_.Name -match "$($vhd.Name)\.vhd"}
+            if($vhdBlob)
             {
-                Write-warning "Found more than one vhd for $($vhd.Name) across all storage accounts"
+                Write-Verbose "Found matching Blob for VHD [$($vhdBlob.Name)]"
+                $remainingDisks = $remainingDisks -ne $vhd.name
+                $rmVMDataDiskDetails += $vhdBlob
+                if($vhdBlob.GetType() -eq 'Object[]')
+                {
+                    Write-warning "Found more than one vhd for $($vhd.Name) in storage account"
+                }
             }
         }
+
         #Check if the number of VHDs found matches the number of disks attached to the VM
-        if($azureRMVM.StorageProfile.DataDisks.Count -ne $rmVMDataDiskDetails.Count)
+        if($azureRMVM.StorageProfile.DataDisks.Count -eq $rmVMDataDiskDetails.Count)
+        {
+            Return $rmVMDataDiskDetails
+        }
+        else
+        {
+            Write-Verbose "Unable to find all VHDs in OS Disk storage account [$osDiskStorageAccountName]"
+            Write-Verbose "Checking other storage accounts"
+        }
+
+        #Check other storage accounts if we cannot find all the VHDs in the same storage account as the OS disk.
+        $storageAccounts = $storageAccounts | Where-Object {$_.StorageAccountName -ne $osDiskStorageAccountName}
+        foreach($vhd in $remainingDisks)
+        {
+            $vhdBlob = $storageAccounts | Get-AzureStorageContainer | Get-AzureStorageBlob | Where-Object {$_.Name -match "$($vhd.Name)\.vhd$"}
+            if($vhdBlob)
+            {
+                Write-Verbose "Found matching Blob for VHD [$($vhdBlob.Name)]"
+                $rmVMDataDiskDetails += $vhdBlob
+                if($vhdBlob.GetType() -eq 'Object[]')
+                {
+                    Write-warning "Found more than one vhd for $($vhd.Name) in storage account"
+                }
+            }
+        }
+
+        if($rmVMDataDiskDetails.Count -gt $azureRMVM.StorageProfile.DataDisks.Count)
         {
             Throw "Number of Vhds matching blob name exceeds number of disks attached to VM"
-        }
+        }        
     }
     Return $rmVMDataDiskDetails
 }
